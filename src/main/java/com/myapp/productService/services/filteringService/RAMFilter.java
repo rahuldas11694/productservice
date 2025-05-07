f600e16c6149da9a0c2d2a3fc0f1b32d34382c30
rahuldas <rahuldas11694@gmail.com>
2025-05-07 06:45:45 +0530

    Load balancing

--- src/main/java/com/myapp/productService/services/filteringService/RAMFilter.java
+++ src/main/java/com/myapp/productService/services/filteringService/RAMFilter.java
@@ -2,12 +2,21 @@ package com.myapp.productService.services.filteringService;
 
 import com.myapp.productService.models.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RAMFilter implements Filter {
 
     @Override
     public List<Product> apply(List<Product> products, List<String> allowedValues) {
-        return List.of();
+        List<Product> ans = new ArrayList<>();
+
+        for (Product product: products) {
+            if (product.getDescription().contains("")) {
+                ans.add(product);
+            }
+        }
+
+        return ans;
     }
 }
